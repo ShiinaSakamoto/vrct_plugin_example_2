d7c3fe4d375594334bca3db936ed1c7644cc4a5e
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-03-25 17:43:12 +0900

    [Update] Move location index to info.json.

--- src/index.jsx
+++ src/index.jsx
@@ -10,11 +10,7 @@ export const init = (plugin_context) => {
         return <MainContainer />;
     };
 
-    plugin_context.registerComponent({
-        plugin_id: "plugin_example_2",
-        location: "main_section",
-        component: EntryComponents,
-    });
+    plugin_context.registerComponent(EntryComponents);
 };
 
 export default init;
